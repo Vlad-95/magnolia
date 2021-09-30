b56d3b03073a5d5ed40d9e4bbea8435958eb6566
Vladislav <vzv@siluetstudio.com>
2021-09-30 16:56:40 +0300

    first commit

---
+++ js/jquery-ready.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    
+    //клик по бургеру
+    // $('.burger').click(function() {
+    //     $('.header__bottom').show("slide", { direction: "right" }, 500);
+    // });
+
+    //закрытие меню
+    // $('.header__bottom .close').click(function() {
+    //     $('.header__bottom').hide("slide", { direction: "right" }, 500);
+    // })
+
+
+    //одинаковая высота новостей
+    // if($('.news').not('.news_detail').length) {
+    //     $('.news__item .news__content').matchHeight();
+    // }
+
+    //одинаковая высота преимуществ
+    // if($('.advantages').length) {
+    //     $('.advantages__item .advantages__text').matchHeight();
+    // }
+
+    //одинаковая высота названий контактов
+    // if($('.contact_page').length) {
+    //     $('.contact__row .contact__item-name').matchHeight();
+    // }    
+});
